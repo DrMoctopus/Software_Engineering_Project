d8311f8fe0abf2a83ec94bd9973de21e3a470402
Will <wmock24@yahoo.com>
2024-04-22 12:07:23 -0500

    Create implementation.py
    initial commit and program prologue

---
+++ implementation.py
@@ -0,0 +1,17 @@
+"""
+Program: Credit Card Fraud Detection System
+Group Members:
+  - William Mock
+  - Christian Tuarez
+  - Valary Musibega
+  - Christian Tuarez
+
+Description:
+  This program is a group project developed by the above-listed group members as part of a collaborative effort.
+  The project aims to implement a credit card fraud detection system with real-time monitoring capabilities.
+  The system is designed to analyze transactional data, identify fraudulent activities, and provide alerts for further investigation.
+
+  Throughout the development process, the group members collaborated on various tasks, including data preprocessing, system design, implementation, and user interface development.
+
+  This prologue serves as documentation for the project, providing an overview of the purpose, objectives, and contributors involved in its development.
+"""
